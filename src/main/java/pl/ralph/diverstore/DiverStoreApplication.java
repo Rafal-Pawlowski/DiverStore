1ea255bae52f3c9b6666cc7868eba469e8621183
Rafal <rafalpawlowski.poczta@gmail.com>
2024-03-27 23:01:10 +0100

    Skopiowano zawartośc DiverShop do DiverStore

---
+++ src/main/java/pl/ralph/diverstore/DiverStoreApplication.java
@@ -0,0 +1,13 @@
+package pl.ralph.diverstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiverStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ralph/diverstore/DiverStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ralph.diverstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
