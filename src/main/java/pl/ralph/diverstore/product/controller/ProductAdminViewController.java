4e44b208bf59a1573d44af5a5357d7459f92ef91
Rafal <rafalpawlowski.poczta@gmail.com>
2024-04-11 00:05:55 +0200

    12. AddProduct feature added

--- src/main/java/pl/ralph/diverstore/product/controller/ProductAdminViewController.java
+++ src/main/java/pl/ralph/diverstore/product/controller/ProductAdminViewController.java
@@ -3,7 +3,9 @@ package pl.ralph.diverstore.product.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.ralph.diverstore.product.domain.model.Product;
 import pl.ralph.diverstore.product.service.ProductService;
 
 @Controller
@@ -22,6 +24,18 @@ public class ProductAdminViewController {
     public String indexView(Model model){
         model.addAttribute("products", productService.findAllProducts());
         return "admin/product/index";
+    }
+
+    @GetMapping("add")
+    public String addView(Model model){
+        model.addAttribute("product", new Product());
+        return "admin/product/add";
+    }
+
+    @PostMapping
+    public String add(Product product){
+        productService.createProduct(product);
+        return "redirect:/admin/products";
     }
 
 }
